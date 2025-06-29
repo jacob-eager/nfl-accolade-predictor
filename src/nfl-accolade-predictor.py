120db019f0dc8f159eacd7e7942ecd504173d28d
jacob-eager <jakeeager.04@gmail.com>
2025-06-29 15:06:04 -0400

    Added basic user input capabilities

--- src/nfl-accolade-predictor.py
+++ src/nfl-accolade-predictor.py
@@ -1,6 +1,57 @@
 
 def main():
-    print("Hello world!")
+    prompt_user()
+
+def prompt_user():
+    # This could probably be replaced with asking for a position, if we decide to use position as an attribute
+    category = input("Is your player on offense or defense? \n")
+
+    if category.lower() == "offense":
+        has_passing_stats = input("Does your player have passing stats? \n")
+        if has_passing_stats.lower() == "yes":
+            pass_yards = int(input("Passing yards: "))
+            pass_attempts = int(input("Pass attempts: "))
+            pass_completions = int(input("Pass completions: "))
+            pass_tds = int(input("Passing TDs: "))
+            pass_ints = int(input("Passing INTs: "))
+        else:
+            pass_yards = 0
+            pass_attempts = 0
+            pass_completions = 0
+            pass_tds = 0
+            pass_ints = 0
+
+        has_receiving_stats = input("Does your player have receiving stats? \n")
+        if has_receiving_stats.lower() == "yes":
+            targets = int(input("Targets: "))
+            receptions = int(input("Receptions: "))
+            receiving_tds = int(input("Receiving TDs: "))
+        else:
+            targets = 0
+            receptions = 0
+            receiving_tds = 0
+
+        has_rushing_stats = input("Does your player have rushing stats? \n")
+        if has_rushing_stats.lower() == 'yes':
+            rushing_yards = int(input("Rushing yards: "))
+            rushing_attempts = int(input("Rushing attempts: "))
+            rushing_tds = int(input("Rushing TDs: "))
+        else:
+            rushing_yards = 0
+            rushing_attempts = 0
+            rushing_tds = 0
+
+    elif category.lower() == 'defense':
+        fumbles_forced = int(input("Fumbles forced: "))
+        fumbles_recovered = int(input("Fumbles recovered: "))
+        fumble_return_tds = int(input("Fumble return TDs: "))
+        assisted_tackles = int(input("Tackle assists: "))
+        solo_tackles = int(input("Solo tackles: "))
+        sacks = float(input("Sacks: "))
+        defensive_ints = int(input("Defensive INTs: "))
+        pick_sixes = int(input("INT return TDs: "))
+
+
 
 if __name__ == "__main__":
     main()
