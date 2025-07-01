01710360d020d88dc06c7ae296c115aa638757f4
Justin-Ashbau <ashbauj1@ferris.edu>
2025-07-01 04:55:47 -0400

    Create decision_tree_defense_train.py

---
+++ src/decision_tree_defense_train.py
@@ -0,0 +1,51 @@
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import pandas as pd
+import csv
+from sklearn.metrics import mean_absolute_error
+from sklearn.multioutput import MultiOutputClassifier
+import joblib
+import matplotlib.pyplot as plt
+
+with open("../data/defense_complete.csv", mode='r') as file:
+    data = csv.DictReader(file)
+    df = pd.DataFrame(data)
+    df = df.drop(columns=["position"])
+    print(df.head())
+
+    X = df [[
+    "position", "solo_tackle", "assist_tackle", "sack", "safety", "interception",
+    "def_touchdown", "fumble_forced", "games_played_season"
+    ]]
+  
+    y = df[["mvp", "dpoy", "allpro"]]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+    dtc = DecisionTreeClassifier(max_depth=None, class_weight='balanced', min_samples_leaf=2)
+
+    moc = MultiOutputClassifier(dtc)
+
+    moc.fit(X_train, y_train)
+
+    # Save the trained model to a file so we can use it to make predictions later
+    joblib.dump(moc, 'defense_model.pkl')
+
+    # Report how well the model is performing
+    print("Model training results:")
+
+    # Report an error rate on the training set
+    mse_train = mean_absolute_error(y_train, moc.predict(X_train))
+    print(f" - Training Set Error: {mse_train}")
+
+    # Report an error rate on the test set
+    mse_test = mean_absolute_error(y_test, moc.predict(X_test))
+    print(f" - Test Set Error: {mse_test}")
+
+    tree.plot_tree(moc.estimators_[2],
+                   feature_names= ["position", "solo_tackle", "assist_tackle", "sack", "safety", "interception",
+                                  "def_touchdown", "fumble_forced", "games_played_season"]
+                   class_names=["mvp", "dpoy", "allpro"])
+
+    plt.show()
